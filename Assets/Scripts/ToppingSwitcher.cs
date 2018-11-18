4bb157b4d9e4e38396c6f3bba56a8a6c04e16fe5
Warren <guileswa@gmail.com>
2018-11-17 19:40:03 -0600

    Scripts Added
    Basic Scripts and Placeholder Cursor Added

---
+++ Assets/Scripts/ToppingSwitcher.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToppingSwitcher : MonoBehaviour 
+{
+    public enum Toppings 
+    {
+        Pepperoni,
+        GreenPepper,
+        Mushroom,
+        Onion,
+        BlackOlive
+    }
+
+    public GameObject[] toppingObjects;
+
+    public Toppings topping;
+
+    public void SwitchTopping(Toppings toppingToSwitch)
+    {
+        topping = toppingToSwitch;
+    }
+}
