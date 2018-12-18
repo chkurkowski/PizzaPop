081ac25f5f978cf3e3d1ed2d739833bcd1a3ace0
jason-marks <44442842+jason-marks@users.noreply.github.com>
2018-12-18 14:57:05 -0600

    Added Keys to Switch Between Toppings
    The 1-5 keys will change Player 1's toppings and the 6-0 keys will change Player 2's toppings.

--- Assets/Scripts/ToppingSwitcher.cs
+++ Assets/Scripts/ToppingSwitcher.cs
@@ -12,6 +12,62 @@ public class ToppingSwitcher : MonoBehaviour
         instance = this;
     }
 
+    public void Update() 
+    {
+        /* Buttons 1-5 on the keyboard are for Player 1*/
+
+        if (Input.GetKeyDown("1"))
+        {
+            SwitchPlayer1Topping(0); // Switch Player 1's topping to Pepperoni
+        }
+
+        if (Input.GetKeyDown("2"))
+        {
+            SwitchPlayer1Topping(1); // Switch Player 1's topping to GreenPepper
+        }
+        
+        if (Input.GetKeyDown("3"))
+        {
+            SwitchPlayer1Topping(2); // Switch Player 1's topping to Mushroom
+        }
+
+        if (Input.GetKeyDown("4"))
+        {
+            SwitchPlayer1Topping(3); // Switch Player 1's topping to Onion
+        }
+
+        if (Input.GetKeyDown("5"))
+        {
+            SwitchPlayer1Topping(4); // Switch Player 1's topping to BlackOlive
+        }
+
+        /* Buttons 6-0 on the keyboard are for Player 2*/
+
+        if (Input.GetKeyDown("6"))
+        {
+            SwitchPlayer2Topping(0); // Switch Player 2's topping to Pepperoni
+        }
+
+        if (Input.GetKeyDown("7"))
+        {
+            SwitchPlayer2Topping(1); // Switch Player 2's topping to GreenPepper
+        }
+
+        if (Input.GetKeyDown("8"))
+        {
+            SwitchPlayer2Topping(2); // Switch Player 2's topping to Mushroom
+        }
+
+        if (Input.GetKeyDown("9"))
+        {
+            SwitchPlayer2Topping(3); // Switch Player 2's topping to Onion
+        }
+
+        if (Input.GetKeyDown("0"))
+        {
+            SwitchPlayer2Topping(4); // Switch Player 2's topping to BlackOlive
+        }
+    }
 
     public enum Toppings 
     { 
@@ -45,7 +101,6 @@ public class ToppingSwitcher : MonoBehaviour
         p2Topping = (Toppings)toppingToSwitch;
         p2ToppingDisplay.sprite = toppingSprites[toppingToSwitch];
     }
-
 
     public Toppings GetPlayer1Topping()
     {
