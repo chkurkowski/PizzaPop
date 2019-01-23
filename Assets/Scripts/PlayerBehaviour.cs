058dfb9f55cda07d0ce20ff6847f2e5679bd6571
Chase Kurkowski <zeke1498@gmail.com>
2019-01-22 18:49:09 -0600

    Rotations added to the toppings as they fly.

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -121,7 +121,7 @@ public class PlayerBehaviour : MonoBehaviour
         }
             
         bullet.GetComponent<ToppingScript>().SetDestination(transform.position);
-
+       
         bullet.GetComponent<ToppingScript>().playerShooter = player;
 
         bullet.transform.localScale = new Vector3(5f, 5f, 5f);
