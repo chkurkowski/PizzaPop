3fdffc6c74ecc4c0b5ba2c520bec35424f7e3200
jason-marks <44442842+jason-marks@users.noreply.github.com>
2019-02-10 17:23:21 -0600

    add demo screen

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,8 +8,11 @@ public class UIManager : MonoBehaviour {
     public GameObject titleScreen;
     public GameObject payoffScreen;
     public GameObject highScoreScreen;
+    public GameObject demoScreen;
     public bool leftStartScreen = false;
     public bool onPayoffScreen = false;
+    public bool onDemoScreen = false;
+    public float secsSinceLastInput = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -31,5 +34,24 @@ public class UIManager : MonoBehaviour {
             highScoreScreen.SetActive(true);
             onPayoffScreen = false;
         }
+
+        if (!Input.anyKeyDown && !leftStartScreen && secsSinceLastInput > 10)
+        {
+            titleScreen.SetActive(false);
+            demoScreen.SetActive(true);
+            onDemoScreen = true;
+            leftStartScreen = true;
+        }
+
+        if (Input.anyKeyDown && onDemoScreen)
+        {
+            titleScreen.SetActive(true);
+            demoScreen.SetActive(false);
+            onDemoScreen = false;
+            leftStartScreen = false;
+            secsSinceLastInput = 0;
+        }
+
+        secsSinceLastInput += Time.deltaTime;
 	}   
 }
