2f075ab25b83d61bb08d8de24bffb66d4f8d6836
Chase Kurkowski <chk@fastmail.com>
2019-02-13 17:18:02 -0600

    New ToppingScoreHandler Script

---
+++ Assets/Scripts/ToppingScoreHandler.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToppingScoreHandler : MonoBehaviour {
+
+	public static ToppingScoreHandler inst;
+
+	#region Singleton
+
+	void Awake()
+	{
+		if(inst == null)
+		{
+			inst = this;
+		}
+	}
+
+	#endregion
+
+	private float pepperoniScore = 100;
+	private float greenPepperScore = 100;
+	private float mushroomScore = 100;
+	private float oliveScore = 100;
+	private float onionScore = 100;
+
+	public float GetScore(string topping)
+	{
+		switch(topping)
+		{
+			case "pepperoni":
+				return pepperoniScore;
+			case "greenpepper":
+				return greenPepperScore;
+			case "mushroom":
+				return mushroomScore;
+			case "olive":
+				return oliveScore;
+			case "onion":
+				return onionScore;
+		}
+	}
+}
