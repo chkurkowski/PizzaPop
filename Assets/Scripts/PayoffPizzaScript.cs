b4f5b49279c2d80584bb08da86ad44aa4c8505c3
jason-marks <44442842+jason-marks@users.noreply.github.com>
2019-02-24 17:25:09 -0600

    hotfix for payoff screen

--- Assets/Scripts/PayoffPizzaScript.cs
+++ Assets/Scripts/PayoffPizzaScript.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PayoffPizzaScript : MonoBehaviour {
+public class PayoffPizzaScript : MonoBehaviour
+{
 
     public GameObject pizzaBox;
     int boxQuotient = 0;
+    int boxQuotient2 = 0;
     public static PayoffPizzaScript payoff;
-
+    public Image[] player1Pizzas;
+    public Image[] player2Pizzas;
 
     void Awake()
     {
@@ -16,46 +20,36 @@ public class PayoffPizzaScript : MonoBehaviour {
 
     public void DetermineGameResults()
     {
-        AudioManager.instance.Stop("Gameplay Music");
+        GameManager.manager.setPlayer1Score(400);
+        GameManager.manager.setPlayer2Score(400);
         GetPlayersScores();
-        StartCoroutine(SpawnPizzaBoxes());
+        StartCoroutine(SpawnPlayer1Boxes());
+        StartCoroutine(SpawnPlayer2Boxes());
     }
 
     void GetPlayersScores()
     {
-        if (GameManager.manager.getPlayer2Score() > GameManager.manager.getPlayer1Score())
-        {
-            boxQuotient = GameManager.manager.getPlayer2Score() / 20;
-        }
-
-        if (GameManager.manager.getPlayer2Score() < GameManager.manager.getPlayer1Score())
-        {
-            boxQuotient = GameManager.manager.getPlayer1Score() / 20;
-        }
+        boxQuotient2 = GameManager.manager.getPlayer2Score() / 20;
+        boxQuotient = GameManager.manager.getPlayer1Score() / 20;
     }
 
-    IEnumerator SpawnPizzaBoxes()
+    IEnumerator SpawnPlayer1Boxes()
     {
-        Debug.Log(boxQuotient);
-        for (int i = 1; i < boxQuotient; i++)
+        Debug.Log("quotient 1: " + boxQuotient);
+        for (int i = 0; i < boxQuotient; i++)
         {
-            Vector2 newPizza = new Vector2(Random.Range(-2, 3), transform.position.y + 10);
-            ObjectPooler.instance.SpawnFromPool("PizzaBox", newPizza, Quaternion.identity);
+            player1Pizzas[i].enabled = true;
             yield return new WaitForSeconds(.3f);
         }
     }
 
-    
-    /* player 1 score is 10000
-     player 2 socre is 5999
-
-    compare players' scores
-    highest one / 20 = quotient
-
-    in a coroutine:
-        for how many times the quotient is
-            spawn new pizza at random x offset, y + small amount, same rotation
-
-
-    */
+    IEnumerator SpawnPlayer2Boxes()
+    {
+        Debug.Log("quotient 2: " + boxQuotient2);
+        for (int i = 0; i < boxQuotient2; i++)
+        {
+            player2Pizzas[i].enabled = true;
+            yield return new WaitForSeconds(.3f);
+        }
+    }
 }
